d70d8d8a8c68c53444bcb3ede7c9256ceb2fd220
oyatu-pepo <oyatu-pepo@users.noreply.github.com>
2020-02-15 18:50:50 +0900

    各ページ、雛形を用意

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Goods from './containers/Goods'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Goods />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
---
+++ src/containers/Complete.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Complete = () => {
+  return (
+    <div>
+      <h1>Complete Page</h1>
+    </div>
+  );
+};
+
+export default Complete;
